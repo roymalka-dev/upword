e0451f94b90fc8a4c8d6026f74c3f379e41411a4
Roy Malka <roy.malka@roy.malka-MBP>
2024-05-01 15:35:59 +0300

    openai error

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -2,10 +2,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import OpenAI from "openai";
 
- const openai = new OpenAI({
-   apiKey: process.env.OPENAI_API_KEY,
- });
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -19,10 +15,14 @@ export default async function handler(
   if (!text) {
     return res.status(400).json({ message: "Text parameter is required" });
   }
-    
+
   const pormpt = `and rephrase the following text to a ${scale} of 10 formality level : ${text}`;
 
   try {
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY || "",
+    });
+
     const stream = await openai.chat.completions.create({
       model: "gpt-3.5-turbo-0125",
       messages: [{ role: "user", content: pormpt }],
